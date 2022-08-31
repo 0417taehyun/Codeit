489c55a350bd7996eb834b188522dede7cba0ca0
0417taehyun <0417taehyun@gmail.com>
2022-08-31 22:39:08 +0900

    :memo: [ 데이터 사이언스 기초 ] 01. 데이터 사이언스 시작하기 : 1. 데이터 사이언스란?

---
+++ STEP 2 데이터 사이언스 기초/01. 데이터 사이언스 시작하기/01.py
@@ -0,0 +1,39 @@
+# 1. 데이터 사이언스란? | 06. 선수 과제
+
+def is_palindrome(word: str) -> bool:
+    start, end = 0, len(word)-1
+    while start < end:
+        if word[start] != word[end]:
+            return False
+        
+        start, end = start+1, end-1
+        
+    return True
+
+
+if __name__ == "__main__":
+    cases: list[dict[str, dict[str, str] | bool]] = [
+        {
+            "input": {"word": "racecar"},
+            "output": True
+        },
+        {
+            "input": {"word": "stars"},
+            "output": False
+        },
+        {
+            "input": {"word": "토마토"},
+            "output": True
+        },
+        {
+            "input": {"word": "kayak"},
+            "output": True
+        },
+        {
+            "input": {"word": "hello"},
+            "output": False
+        }                                
+    ]
+    for case in cases:
+        assert case["output"] == is_palindrome(**case["input"])
+        
